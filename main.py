bc582d55865166373fd6ed111451f325cfe21181
saintares97 <rolonvan@gmail.com>
2024-10-17 13:27:50 -0400

    refactored main.py and added a count function

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_location = "books/frankenstein.txt" # stores the book's relative path in a variable
+    text = read_book(book_location)
+    word_count = count_words(text)
+    print("The total amount of words in this book is", word_count)
+
+# opens the text file, reads it, and returns text
+def read_book(path):
+    with open(path) as f:
+        return f.read()
+
+# counts the number of words in the book
+def count_words(text):
+    count = 0
+    for word in text.split():
+        count += 1
+    return count
 
 main()
